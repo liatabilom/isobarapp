5f60811959159fb20d64f5266c1539151897d0dd
Lia <lia.tabilo@gmail.com>
2019-12-22 02:02:02 +0000

    Add commentBox

---
+++ src/ably.js
@@ -0,0 +1,3 @@
+import { Realtime } from 'ably/browser/static/ably-commonjs.js';
+
+window.Ably = new Realtime('Rt09mQ.324-7w:P9_PhNXffQjURthU');
--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,13 @@
-import React, { component } from 'react';
+import React, { Component } from 'react';
+import CommentBox from './CommentBox';
 
 class App extends Component {
 	render() {
-		return <div className="App"></div>;
+		return (
+			<div className="App">
+				<CommentBox />
+			</div>
+		);
 	}
 }
 
